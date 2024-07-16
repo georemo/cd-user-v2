7f8b269bf3f16c1449ccad11e402b0af5054cc0b
“georemo” <george.oremo@gmail.com>
2024-07-16 19:00:17 +0300

    -

--- projects/cd-user/webpack.config.js
+++ projects/cd-user/webpack.config.js
@@ -9,6 +9,8 @@ module.exports = withModuleFederationPlugin({
     './PagesModule': './projects/cd-user/src/app/pages/pages.module.ts',
     './LoginComponent': './projects/cd-user/src/app/account/auth/login/login.component.ts',
     './UserFrontModule': './projects/cd-user/src/app/modules/user/user-front.module.ts',
+    './GroupModule': './projects/cd-user/src/app/modules/group/group.module.ts',
+    './GroupMemberModule': './projects/cd-user/src/app/modules/group-member/group-member.module.ts',
   },
 
   shared: {
